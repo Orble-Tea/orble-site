02308ee3386a0ab9479f73b6ec11a88ad73227c9
Shrey Pasricha <shrey.pasricha@gmail.com>
2023-01-23 03:54:56 -0500

    Cleaned up scrolly and added more info.

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,12 @@
-module.exports = {
+/** @type {import('prettier').Options;} **/
+const config = {
   plugins: [
     "prettier-plugin-astro",
     "prettier-plugin-tailwindcss", // MUST come last
   ],
   pluginSearchDirs: false,
+  singleQuote: false,
+  // htmlWhitespaceSensitivity: "strict"
 };
+
+module.exports = config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Lato", ...defaultTheme.fontFamily.sans],
-        heading: ["Staatliches", ...defaultTheme.fontFamily.sans]
+        heading: ["Staatliches", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: "var(--color-primary)",
@@ -23,6 +24,24 @@ module.exports = {
       },
       borderColor: {
         default: "var(--color-border)",
+      },
+      animation: {
+        "fade-move-down": "fade-move-down 4s ease-in-out infinite",
+      },
+      keyframes: {
+        "fade-move-down": {
+          "100%": {
+            transform: "translate(0%, 20%)",
+            opacity: 0,
+          },
+          "50%": {
+            opacity: 1,
+          },
+          "0%": {
+            transform: "translate(0%, -20%)",
+            opacity: 0,
+          },
+        },
       },
     },
   },
