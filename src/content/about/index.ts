1ebef03ea97a6723e8c88ea561de812d23e6be26
Ann Kang <annkang@andrew.cmu.edu>
2025-10-05 14:33:33 -0400

    update member info

--- src/content/about/index.ts
+++ src/content/about/index.ts
@@ -1,17 +1,7 @@
-import jaiman from '~/assets/images/team-headshots/jaiman.jpg' 
-import nastassja from '~/assets/images/team-headshots/nastassja.jpg'
-import adam from '~/assets/images/team-headshots/adam.jpg'
-import tom from '~/assets/images/team-headshots/tom.png'
-import janavi from '~/assets/images/team-headshots/janavi.png'
-import maya from '~/assets/images/team-headshots/maya.jpg'
-import vidhya from '~/assets/images/team-headshots/vidhya.jpg'
-import josue from '~/assets/images/team-headshots/josue.jpg'
-import danshu from '~/assets/images/team-headshots/danshu.jpg'
-import ella from '~/assets/images/team-headshots/ella.jpg'
-import kyle from '~/assets/images/team-headshots/kyle.png'
-import sammi from '~/assets/images/team-headshots/sammi.jpg'
-import jean from '~/assets/images/team-headshots/jean.png'
-import maisha from '~/assets/images/team-headshots/maisha.jpg'
+import corina from '~/assets/images/team-headshots/corina.png' 
+import nastassja from '~/assets/images/team-headshots/nastassja.png'
+import ann from '~/assets/images/team-headshots/ann.png'
+import krishna from '~/assets/images/team-headshots/krishna.png' 
 
 export const teamMembers = [
   {
@@ -21,81 +11,23 @@ export const teamMembers = [
     bio: "",
   },
   {
-    name: "Jaiman Munshi",
+    name: "Corina Chen",
+    title: "CTO, Mechanical Engineer",
+    image: corina,
+    bio: "",
+  },
+  {
+    name: "Krishna Chaithanya Reddy Palpaty",
+    title: "Mechanical Engineer",
+    image: krishna,
+    bio: "",
+  },
+  {
+    name: "Ann Kang",
     title: "Software Engineer",
-    image: jaiman,
+    image: ann,
     bio: "",
   },
-  {
-    name: "Adam Bhaloo",
-    title: "Engineer and Business Development",
-    image: adam,
-    bio: "",
-  },
-  {
-    name: "Tom Schmitz",
-    title: "Embedded Developer",
-    image: tom,
-    bio: "",
-  },
-  {
-    name: "Janavi Chadha",
-    title: "Project Manager",
-    image: janavi,
-    bio: "",
-  },
-  {
-    name: "Maya Pasricha",
-    title: "Quality Assurance Engineer",
-    image: maya,
-    bio: "",
-  },
-  {
-    name: "Vidhya Kalimani",
-    title: "Mechanical Engineer",
-    image: vidhya,
-    bio: "",
-  },
-  {
-    name: "Josué León",
-    title: "Mechanical Engineer",
-    image: josue,
-    bio: "",
-  },
-  {
-    name: "Danshu Shen",
-    title: "Electrical Engineer",
-    image: danshu,
-    bio: "",
-  },
-  // {
-  //   name: "Ella Phillippy",
-  //   title: "Marketing Fellow",
-  //   image: ella,
-  //   bio: "",
-  // },
-  // {
-  //   name: "Kyle Fukumoto",
-  //   title: "Business Development Fellow",
-  //   image: kyle,
-  //   bio: "",
-  // },
-  // {
-  //   name: "Sammi Xu",
-  //   title: "Business Development Fellow",
-  //   image: sammi,
-  //   bio: "",
-  // },
-  // {
-  //   name: "Jean Ee Chua",
-  //   title: "Business Development Fellow",
-  //   image: jean,
-  //   bio: "",
-  // },
-  {
-    name: "Maisha Takahashi",
-    title: "Software Intern",
-    image: maisha,
-    bio: "",
-  }
+
+
 ];
